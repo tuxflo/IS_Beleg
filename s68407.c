5f97671f434ed1beaa895c409e975d7cd24b046e
tuxflo <flo.hennig@gmail.com>
2016-06-21 16:14:50 +0200

    first stuff

--- s68407.c
+++ s68407.c
@@ -12,7 +12,6 @@ void handleErrors(void)
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
   unsigned char *iv, unsigned char *plaintext)
 {
-  EVP_CIPHER_CTX *ctx;
 
   int len;
 
@@ -26,7 +25,7 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
    * In this example we are using 256 bit AES (i.e. a 256 bit key). The
    * IV size for *most* modes is the same as the block size. For AES this
    * is 128 bits */
-  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ecb(), NULL, key, iv))
     handleErrors();
 
   /* Provide the message to be decrypted, and obtain the plaintext output.
@@ -94,53 +93,83 @@ int main (void)
    */
 
   /* A 256 bit key */
-  unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+  unsigned char key[32];
 
-  /* A 128 bit IV */
-  unsigned char *iv = (unsigned char *)"01234567890123456";
 
-  /* Message to be encrypted */
-  unsigned char *plaintext =
-                (unsigned char *)"The quick brown fox jumps over the lazy dog";
+  // fopen corrupted file
+  FILE *corrupted;
 
+  corrupted = fopen("./s68407-src-cipher.bin", "r");
+  if(corrupted == NULL)
+  {
+    printf("unable to open file\n");
+    exit(-1);
+  }
+
+  //copy key in array using fread
+  FILE * key = fopen("s68407-corrupt-src-key.bin", "r");
+  long length;
+  unsigned char *buffer;
+  if(key == NULL)
+  {
+    printf("unable to open key file\n");
+    exit(-1);
+  }
+  fseek (key, 0, SEEK_END);
+  length = ftell (key);
+  fseek (key, 0, SEEK_SET);
+  buffer = malloc (length);
+  if (!buffer)
+  {
+    printf("error using malloc\n");
+    exit(-2);
+  }
+  fread (buffer, 1, length, key);
+  fclose (key);
+  printf("key: %s\n", key);
+
+  EVP_CIPHER_CTX ctx;
+  
+  //rückgabewert
+  const EVP_CIPHER *chifer;
+  //zum Vergleichen der Key sizes
+  //
+  EVP_CIPHER_key_length == 32;
+  //bei arbeit & Operator verwenden
   /* Buffer for ciphertext. Ensure the buffer is long enough for the
    * ciphertext which may be longer than the plaintext, dependant on the
    * algorithm and mode
    */
-  unsigned char ciphertext[128];
+  int byte = 0;
+  for(byte = 0; byte < 256; byte++)
+  {
+    key[30] = byte;
 
-  /* Buffer for the decrypted text */
-  unsigned char decryptedtext[128];
+    //Decrypt init
+    //Datei in Puffer laden
+    //Decrypt update (dazu wird ausgangspuffer benötigt)
+    //erste 4 Byte in pdf sind "%PDF"
+    //Vergleich mit memcmp durchführen falls nicht pdf, continue
+    //Ausgabepuffer in Datei schreiben
+    //EVP_Decrypt_final
+    //Ausgabepuffer erneut in Datei schreiben
+    //EVP Cipher Cleanup
+    //EVP_MD context
+    //MD value als char array länge 28 Byte 
+    unsigned char value[28];
+    //Datei laden
+    //MD_CTX init
+    //EVP_DigestInit
+    //EVP_DigestUpdate
+    //EVP_DigestFinal
 
-  int decryptedtext_len, ciphertext_len;
 
-  /* Initialise the library */
-  ERR_load_crypto_strings();
-  OpenSSL_add_all_algorithms();
-  OPENSSL_config(NULL);
-
-  /* Encrypt the plaintext */
-  ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                            ciphertext);
-
-  /* Do something useful with the ciphertext here */
-  printf("Ciphertext is:\n");
-  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-  /* Decrypt the ciphertext */
-  decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-    decryptedtext);
-
-  /* Add a NULL terminator. We are expecting printable text */
-  decryptedtext[decryptedtext_len] = '\0';
-
-  /* Show the decrypted text */
-  printf("Decrypted text is:\n");
-  printf("%s\n", decryptedtext);
-
-  /* Clean up */
-  EVP_cleanup();
-  ERR_free_strings();
-
+    //4. dest_key einlesen zum Schlüssel holen
+    //output file
+    //context anlegen
+    //initialisieren
+    //CipherUpdate zu verschlüsseln
+    //CipherFinal
+  }
   return 0;
 }
